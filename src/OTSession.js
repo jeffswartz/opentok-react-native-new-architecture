536645da45b1d43e905d22f45c2842a4b30908ce
Jeff Swartz <jeff.swartz@vonage.com>
2025-01-26 14:03:32 -0800

    Add old types from otrn 2.28

--- src/OTSession.js
+++ src/OTSession.js
@@ -1,50 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 import PropTypes from 'prop-types';
-import { OT } from './OT';
 
 export default class OTSession extends Component {
-  eventHandlers = {};
-  async initSession(apiKey, sessionId, token) {
-    OT.onSessionConnected((event: SessionConnectEvent) => {
-      this.eventHandlers.sessionConnected(event);
-      if (Object.keys(this.props.signal).length > 0) {
-        this.signal(this.props.signal);
-      }
-    });
-    OT.initSession(apiKey, sessionId, {});
-    OT.onStreamCreated((event) => {
-      this.eventHandlers.streamCreated(event);
-    });
-    /* TO DO:
-    OT.onStreamDestroyed((event) => {
-      this.eventHandlers.onStreamDestroyed(event);
-    });
-    */
-    OT.onSignalReceived((event) => {
-      this.eventHandlers.signal(event);
-    });
-
-    OT.onSessionError((event) => {
-      this.eventHandlers.error(event);
-    });
-    OT.connect(sessionId, token);
-  }
 
   constructor(props) {
     super(props);
-    this.eventHandlers = props.eventHandlers;
-    this.initComponent(props.eventHandlers);
+    //
   }
 
   initComponent = () => {
-    this.initSession(this.props.apiKey, this.props.sessionId, this.props.token);
-    this.eventHandlers.sessionConnected = this.props.eventHandlers?.sessionConnected;
+    //
   };
 
   signal(signalObj) {
-    OT.sendSignal(this.props.sessionId, signalObj.type, signalObj.data);
+    // OT.sendSignal(this.props.sessionId, signalObj.type, signalObj.data);
   }
 
   render() {
@@ -52,11 +23,12 @@ export default class OTSession extends Component {
     if (children && sessionId && apiKey && token) {
       return (
         <View style={style}>
+          <Text>foo</Text>
           { children }
         </View>
       );
     }
-    return <View />;
+    return <View><Text>foobar</Text></View>;
   }
 }
 
